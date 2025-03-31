b99443026aff1caf0c2d50b3c16f4c2da67d2f72
Huy Ph <huypmg.16@gmail.com>
2025-03-31 22:37:08 +0700

    finish structure - theme - splash screen

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBg = Color(0xffF2F2F2);
+  static const darkBG = Color(0xff1C1B1B);
+
+  static const grey = Color(0xffBEBEBE);
+  static const lightNav = Color(0xffFFFFFF);
+  static const darkNav = Color(0xff343434);
+}
