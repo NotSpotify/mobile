aad13a6210669627def4b60a447faf70ff44c2b9
Huy Phan <huy.pmg16@gmail.com>
2025-05-29 12:14:45 +0700

    hotfix: baseUrl

--- lib/core/config/enum/baseURL.dart
+++ lib/core/config/enum/baseURL.dart
@@ -1,4 +1,4 @@
 class Baseurl {
-  // static const String baseUrl = "https://backend-ai-vk8i.onrender.com/";
-  static const String baseUrl = "http://10.0.2.2:8000/";
+  static const String baseUrl = "https://backend-ai-vk8i.onrender.com/";
+  // static const String baseUrl = "http://10.0.2.2:8000/";
 }
